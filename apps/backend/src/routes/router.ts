e733c7c3860263a9bd2c0152b112bcf78cb0ca09
Diabene <addodiabene69@gmail.com>
2024-04-25 16:24:34 +0000

    refactor(app)!: Remove turbo

--- apps/backend/src/routes/router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { VERSION_ONE_ROUTER } from "./v1";
-
-const router: Router = Router();
-
-router.use("/api/v1", VERSION_ONE_ROUTER);
-
-export default router;
