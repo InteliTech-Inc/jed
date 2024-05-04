8e324fee9c80aad018e89c8a28928bb280c12dc9
Elabo <ellaboevans@gmail.com>
2024-05-04 01:06:24 +0000

    fix(app): Fix image url

--- src/app/(system)/events/page.tsx
+++ src/app/(system)/events/page.tsx
@@ -1,11 +1,38 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/supabase";
+import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-async function EventsPage() {
-  const { data, error } = await db.from("events").select();
-  console.log("Events", data);
+interface Event {
+  id: string;
+  name: string;
+  img_url: string;
+}
+
+function EventsPage() {
+  const [events, setEvents] = useState<null | Event[]>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        setIsLoading(true);
+        const { data: events, error } = await db.from("events").select("*");
+        if (events) {
+          setEvents(events);
+          setIsLoading(false);
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.stack);
+        }
+      }
+    }
+    fetchEvents();
+  }, []);
+
   return (
     <div>
       <div className="flex justify-between items-center gap-5">
@@ -22,17 +49,29 @@ async function EventsPage() {
       </div>
       <br />
       <div>
-        {data?.length === 0 ? (
+        {events?.length === 0 ? (
           <div>No Events to show</div>
         ) : (
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
-            {data?.map((event) => {
-              return (
-                <div className="border p-3 rounded-xl" key={event.id}>
-                  <h5 className="font-semibold">{event.name}</h5>
-                </div>
-              );
-            })}
+            {isLoading ? (
+              <h1>Fetching Data...</h1>
+            ) : (
+              events?.map((event) => {
+                return (
+                  <div className="border p-3 h-fit rounded-xl" key={event.id}>
+                    <h5 className="font-semibold">{event.name}</h5>
+                    <Image
+                      src={`https://cbboxofzpwjfpihaoyhn.supabase.co/storage/v1/object/public/events/${event.img_url}`}
+                      alt={`${event.name}`}
+                      width={2000}
+                      height={2000}
+                      className="rounded-md w-full h-full my-4"
+                      priority
+                    />
+                  </div>
+                );
+              })
+            )}
           </div>
         )}
       </div>
