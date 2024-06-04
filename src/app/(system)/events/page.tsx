f61bbc950af33a38b8ac0ece7f6d125269a7facb
Elabo <ellaboevans@gmail.com>
2024-06-04 23:00:28 +0000

    fix(app): Use React Suspense while streaming data

--- src/app/(system)/events/page.tsx
+++ src/app/(system)/events/page.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/ui/button";
 import { dbServer } from "@/lib/supabase";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { Suspense } from "react";
 import { cookies } from "next/headers";
+import EventsCard from "./components/events_card";
+import Spinner from "@/components/rotating_lines";
 
 async function EventsPage() {
   const db = dbServer(cookies);
@@ -11,13 +13,6 @@ async function EventsPage() {
   const {
     data: { user },
   } = await db.auth.getUser();
-
-  const { data: events } = await db
-    .from("events")
-    .select("*")
-    .eq("user_id", user?.id!);
-
-  console.log(events);
 
   return (
     <div className="p-6">
@@ -35,40 +30,15 @@ async function EventsPage() {
       </div>
       <br />
       <div>
-        {events?.length === 0 ? (
-          <div>No Events to show</div>
-        ) : (
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3">
-            {events?.map((event) => {
-              return (
-                <Link
-                  href={`/events/${event.id}`}
-                  key={event.id}
-                  className="border p-3 h-fit rounded-xl"
-                >
-                  <section className=" h-64">
-                    <Image
-                      src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${event.img_url}`}
-                      alt={`${event.name}'s image`}
-                      width={2000}
-                      height={2000}
-                      className="rounded-md  w-full h-full object-cover object-center"
-                      priority
-                    />
-                  </section>
-                  <h5 className="font-semibold py-2">{event.name}</h5>
-                  <section className="py-4 w-full">
-                    <span className=" ">
-                      <p className="border w-fit text-sm bg-green-100 border-secondary rounded-full px-4 py-1">
-                        {event.is_completed ? "Completed" : "In Progress"}
-                      </p>
-                    </span>
-                  </section>
-                </Link>
-              );
-            })}
-          </div>
-        )}
+        <Suspense
+          fallback={
+            <div className=" w-full grid place-content-center">
+              <Spinner />
+            </div>
+          }
+        >
+          <EventsCard id={user?.id!} />
+        </Suspense>
       </div>
     </div>
   );
