0e123a9db44844870d4ebdf659c583580a25a746
Elabo <ellaboevans@gmail.com>
2024-06-05 01:59:15 +0000

    fix(app): Metadata title

--- src/app/(landing)/all_events/[id]/nominees/page.tsx
+++ src/app/(landing)/all_events/[id]/nominees/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import CategoryNomineeCard from "./components/category_nominee";
+import { Metadata } from "next";
 
 type Props = {};
+
+export const metadata: Metadata = {
+  title: "Nominees",
+};
 
 export default function CategoryNominees({}: Props) {
   return <CategoryNomineeCard />;
