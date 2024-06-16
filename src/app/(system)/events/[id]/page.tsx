45d5fbde1cd1b641e46891580ad29f0fcd198f8f
Elabo <ellaboevans@gmail.com>
2024-06-16 17:55:44 +0000

    feat(app): Delete Category feature

--- src/app/(system)/events/[id]/page.tsx
+++ src/app/(system)/events/[id]/page.tsx
@@ -9,15 +9,6 @@ type Props = {
 export default async function SingleEvent({ params: { id } }: Props) {
   return (
     <section className="p-4">
-      <section className=" mb-4">
-        <p className=" text-4xl text-neutral-700 mb-2 font-semibold">
-          Event details
-        </p>
-        <p className=" text-neutral-600">
-          View and edit the details of your event
-        </p>
-      </section>
-
       <Suspense
         fallback={
           <div className=" w-full grid place-content-center">
