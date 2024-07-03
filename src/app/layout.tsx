feefcc86649d5f515780e2a9bdb10de596b1eb98
Elabo <ellaboevans@gmail.com>
2024-07-03 12:37:27 +0000

    feat(app): Added couple of missing metadata tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,24 +20,32 @@ const openGraphImagesAbsolute = [
   },
 ];
 
+const description =
+  "Discover all the essential tools for creating memorable events, including ticketing, nomination filing, and voting for nominees, designed to be user-friendly for both organizers and attendees.";
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://jed-event.com"),
   title: {
     default: "JED",
     template: "JED - %s",
   },
-  description:
-    "Discover all the essential tools for creating memorable events, including ticketing, nomination filing, and voting for nominees, designed to be user-friendly for both organizers and attendees.",
+  description,
   twitter: {
+    description,
     card: "summary_large_image",
   },
+
   alternates: {
-    canonical: "/",
+    canonical: "https://jed-event.com",
     languages: {
       "en-US": "/en-US",
     },
   },
   openGraph: {
+    siteName: "JED",
+    description,
+    authors: ["Evans Elabo", "Addo Yaw Diabene", "Joshua Richardson Owusu"],
+    url: "https://jed-event.com",
     images: openGraphImagesAbsolute,
   },
 };
