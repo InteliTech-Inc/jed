37795a773ad4a15885ad31d08cdfc45634f88843
Diabene <addodiabene69@gmail.com>
2024-07-05 22:32:52 +0000

    fix ui and shorten url

--- src/app/(system)/events/[id]/nominations/components/nominations_table.tsx
+++ src/app/(system)/events/[id]/nominations/components/nominations_table.tsx
@@ -11,20 +11,24 @@ export default async function NominationsTable({ id }: NominationsTableProps) {
   const db = dbServer(cookies);
   const { data: data_nominations, error } = await db
     .from("nominations")
-    .select("*, categories(category_name)")
+    .select("*, categories(category_name), events(name)")
     .eq("event_id", id);
+
+  const { data } = await db.from("events").select("name").single();
 
   if (!data_nominations) return [];
 
-  const linkURL =
-    process.env.NODE_ENV === "production"
-      ? process.env.NEXT_PUBLIC_LIVE_URL
-      : process.env.NEXT_PUBLIC_DEV_URL;
+  const linkURL = process.env.NEXT_PUBLIC_LIVE_URL;
   const url = `${linkURL}nom?id=${id}`;
 
   return (
     <div>
-      <DataTable data={data_nominations} columns={columns} url={url} />
+      <DataTable
+        data={data_nominations}
+        columns={columns}
+        url={url}
+        event_name={data?.name}
+      />
     </div>
   );
 }
