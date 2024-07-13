5dbe414113bca48c80ff43b005e98d4b14bcfa91
Elabo <ellaboevans@gmail.com>
2024-07-13 18:51:29 +0000

    fix(app): Removed some files

--- src/lib/token.ts
+++ src/lib/token.ts
@@ -1,20 +1,6 @@
 import jwt from "jsonwebtoken";
-import fs from "fs";
-import path from "path";
 
 export function generateToken() {
-  const privateKeyPath = path.resolve(
-    process.cwd(),
-    "src/junipay_privatekey.key"
-  );
-
-  let privateKey;
-  try {
-    privateKey = fs.readFileSync(privateKeyPath, "utf8");
-  } catch (err) {
-    throw err;
-  }
-
   let token;
   try {
     token = jwt.sign(
