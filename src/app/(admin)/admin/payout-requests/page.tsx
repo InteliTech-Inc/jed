10d3caf3dabbd8bcaa250fc67cab203239a644fa
Elabo <ellaboevans@gmail.com>
2024-09-05 19:37:01 +0000

    fix(app): Fixing payout builds

--- src/app/(admin)/admin/payout-requests/page.tsx
+++ src/app/(admin)/admin/payout-requests/page.tsx
@@ -1,14 +1,37 @@
-import PayoutTable from "./components/payout_table";
 import { Suspense } from "react";
 import Spinner from "@/components/spinner";
 import { dbServer } from "@/lib/supabase";
 import { cookies } from "next/headers";
-export default async function AdminPayoutsPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+import { DataTable, PayoutResponse } from "./components/data_table";
+import { columns } from "./components/columns";
+export default async function AdminPayoutsPage() {
   const db = dbServer(cookies);
+
+  // Fetch all requested payouts
+  const { data: payouts } = await db
+    .from("payouts")
+    .select("*")
+    .eq("is_payout_request", true);
+
+  // Fetch events from which payouts are available
+  const { data: events } = await db.from("events").select("*");
+
+  // Create a DataTable with the transformed payout
+  const transformedPayouts = payouts?.map((payout) => {
+    const event = events?.find((event) => event.id === payout?.event_id!);
+    return {
+      id: String(payout.id),
+      amount: String(payout.amount),
+      payment_method: payout.payment_method,
+      account_number: payout.account_number,
+      account_name: payout.account_name,
+      provider: payout.provider,
+      created_at: new Date(payout.created_at).toLocaleString(),
+      updated_at: new Date(payout.created_at).toLocaleString(),
+      transaction_status: payout.is_paid,
+      event_name: event?.name,
+    };
+  });
 
   return (
     <section className="p-4">
@@ -28,7 +51,10 @@ export default async function AdminPayoutsPage({
           </div>
         }
       >
-        <PayoutTable />
+        <DataTable
+          data={transformedPayouts as unknown as PayoutResponse[]}
+          columns={columns}
+        />
       </Suspense>
     </section>
   );
