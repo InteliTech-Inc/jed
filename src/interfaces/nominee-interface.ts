bae1a0c60473d44946c5a90c20513bf35e4bc512
Evans <ellaboevans@gmail.com>
2024-10-11 02:13:34 +0000

    refactor(app): Add nominee interface for type checking

---
+++ src/interfaces/nominee-interface.ts
@@ -0,0 +1,11 @@
+interface Nominee {
+  category_id: string;
+  code: string;
+  created_at: string;
+  event_id: string;
+  full_name: string;
+  id: string;
+  img_public_id: string;
+  img_url: string;
+  user_id: string;
+}
