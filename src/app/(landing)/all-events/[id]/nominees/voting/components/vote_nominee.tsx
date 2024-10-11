4e287b482d20e01377f9b7ee559bf680dc5ee1d8
Evans <ellaboevans@gmail.com>
2024-10-11 02:08:20 +0000

    refactor(app): Integrate the nominee voting view with new api

--- src/app/(landing)/all-events/[id]/nominees/voting/components/vote_nominee.tsx
+++ src/app/(landing)/all-events/[id]/nominees/voting/components/vote_nominee.tsx
@@ -1,30 +1,35 @@
+"use client";
 import Image from "next/image";
 import PaystackPayment from "@/components/paystack_payment";
 import BackButton from "@/components/back";
 
-import { getNominee } from "@/lib/server_endpoints";
 import { getVotingPeriodMessage } from "@/lib/utils";
 
-
 import ShareLink from "./share_link";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvent } from "@/actions/events";
+import { getVotingRecord } from "@/actions/voting";
 
-type Nominee = {
-  id: string;
-  category_id: string | null;
-  full_name: string | null;
-  img_url: string | null;
-  code: string | null;
-  event_id: string | null;
-};
+export default function VoteNomineePage() {
+  const { data: votingNominee } = useQuery<Nominee>({
+    queryKey: ["votingNominee"],
+    staleTime: 1000 * 60 * 5,
+    refetchInterval: 1000 * 60 * 5,
+  });
 
-type Props = {
-  votingNominee: Nominee;
-};
+  const { data: votes } = useQuery({
+    queryKey: ["votes"],
+    queryFn: async () => await getVotingRecord(),
+    refetchOnMount: "always",
+  });
 
-export default async function VoteNomineePage({ votingNominee }: Props) {
-  const info = await getNominee(votingNominee.code || "");
+  const { data: event } = useQuery({
+    queryKey: ["event"],
+    queryFn: async () => await fetchEvent(votingNominee?.event_id!),
+    refetchInterval: 60 * 1000,
+  });
 
-  const endingDate = info.nomineeEvent?.voting_period as {
+  const endingDate = event?.voting_period as {
     start_date: string;
     end_date: string;
   };
@@ -50,8 +55,8 @@ export default async function VoteNomineePage({ votingNominee }: Props) {
         <div className="border rounded-lg p-3 w-full md:w-[25rem] md:h-[30rem]">
           <div className="h-full w-full mb-4">
             <Image
-              className="h-full w-full object-cover object-bottom rounded-lg"
-              src={`${process.env.NEXT_PUBLIC_IMAGE_BASE_URL}/${votingNominee?.img_url}`}
+              className="h-full w-full object-cover object-top rounded-lg"
+              src={votingNominee?.img_url || ""}
               width={2000}
               height={2000}
               alt={votingNominee?.full_name || "Event"}
@@ -63,16 +68,29 @@ export default async function VoteNomineePage({ votingNominee }: Props) {
           <p className=" text-3xl lg:text-4xl font-bold mb-3">
             Vote for {votingNominee?.full_name}
           </p>
-          <p className="text-gray-700">
-            Nominee's Code:{" "}
-            <span className="font-bold text-lg">{votingNominee?.code}</span>{" "}
-          </p>
+          <div className="flex gap-x-4 items-center">
+            <p className="text-gray-700">
+              Nominee's Code:{" "}
+              <span className="font-bold text-lg">{votingNominee?.code}</span>{" "}
+            </p>
+
+            {votes &&
+            votes.length > 0 &&
+            votes[0].nominee_id === votingNominee?.id ? (
+              <p className="text-gray-700">
+                Current Votes:{" "}
+                <span className="font-bold text-lg text-secondary">
+                  {votes[0].count}
+                </span>{" "}
+              </p>
+            ) : null}
+          </div>
           <p className="text-neutral-600">
             You may directly vote offline using the USSD CODE{" "}
             <span className="text-black text-md font-bold">*928*121#</span> and
             Nominee Code
           </p>
-          <PaystackPayment id={votingNominee.id} />
+          <PaystackPayment />
         </div>
       </div>
     </section>
